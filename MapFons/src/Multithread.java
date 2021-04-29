61d33c3dd768caa2ef438c8f52b1fc7901569cb2
FonsDeLange <fons.de.lange@hva.nl>
2021-04-29 12:47:59 +0200

    thread class en oefeningen met thread toegevoegd

---
+++ MapFons/src/Multithread.java
@@ -0,0 +1,9 @@
+public class Multithread extends Thread{
+    public void run(){
+        try{
+            System.out.println("Thread " + Thread.currentThread().getId() + " is running");
+        } catch (Exception e){
+            System.out.println("Exception is caught");
+        }
+    }
+}
