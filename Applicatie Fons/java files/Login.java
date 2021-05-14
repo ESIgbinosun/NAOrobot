d67d4046c85453c41fe28a28807ceb73e43f9dc9
Fons de Lange <fons.de.lange@hva.nl>
2021-05-14 11:45:04 +0000

    Upload New File

---
+++ Applicatie Fons/java files/Login.java
@@ -0,0 +1,66 @@
+/**
+ * Login page view
+ *
+ * Date   17-04-2021
+ * Author Diego Brandjes
+ */
+
+package com.example.nao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+
+    // Fullscreen
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
+    public void nameSafe (View v){
+
+        Button a = findViewById(R.id.button6);
+        EditText usernameEditText = (EditText) findViewById(R.id.naam);
+        String usernaam = usernameEditText.getText().toString();
+        if (usernaam.matches("")) {
+            Toast.makeText(this, "You did not enter a username", Toast.LENGTH_SHORT).show();
+            return;
+        }else if (usernaam.matches("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$")) {
+            a.setText("Thank you!");
+        }else {
+            Toast.makeText(this, "Please enter a normal username", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        a.postDelayed(new Runnable() {
+            @Override
+            public void run() {}
+        },3000);
+
+        Intent i = new Intent(this, Home.class);
+        i.putExtra("KEY", usernaam);
+        startActivity(i);
+    }
+}
